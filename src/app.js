bde01cf7c75a84592546652a60443a8c84d173cc
moreirense <fmorera2015@gmail.com>
2021-04-22 18:20:31 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generator();
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let generator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let rm1 = Math.floor(Math.random() * who.length);
+  let rm2 = Math.floor(Math.random() * action.length);
+  let rm3 = Math.floor(Math.random() * what.length);
+  let rm4 = Math.floor(Math.random() * when.length);
+
+  return who[rm1] + " " + action[rm2] + " " + what[rm3] + " " + when[rm4];
 };
